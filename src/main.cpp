8a7722d70656eaf872deff2643875a2a47074d47
RazerFord <desk10567@gmail.com>
2024-02-22 10:25:19 +0300

    fix: fixed tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,1 @@
-#include "lexical-analyzer/token-generator.h"
-
-int main() { 
-  for (auto& token: token_generator::generate_token("../tests/10_test.olg")) {
-    token->print();
-  }
-  return 0; 
-}
+int main() {}
