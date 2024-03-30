01a41e6f299a58c838e3c9b9c43bd7f584dfe268
RazerFord <desk10567@gmail.com>
2024-03-30 23:46:35 +0300

    fix: fixed error handling

--- src/semantic-analyzer/visitor.h
+++ src/semantic-analyzer/visitor.h
@@ -20,52 +20,29 @@ inline error_handling::error_t make_error_t(const details::ast_node& node,
 
 class visitor {
  public:
-  virtual void visit(const details::identifier_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::primary_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::class_name_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::parameter_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::parameters_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::body_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::class_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::program_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::expression_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::variable_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::method_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::constructor_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::assignment_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::while_loop_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::if_statement_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::return_statement_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::arguments_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::this_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::null_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::base_node&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::literal_node<int32_t>&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::literal_node<bool>&,
-                     error_handling::error_handling&) = 0;
-  virtual void visit(const details::literal_node<double_t>&,
-                     error_handling::error_handling&) = 0;
+  virtual void visit(const details::identifier_node&) = 0;
+  virtual void visit(const details::primary_node&) = 0;
+  virtual void visit(const details::class_name_node&) = 0;
+  virtual void visit(const details::parameter_node&) = 0;
+  virtual void visit(const details::parameters_node&) = 0;
+  virtual void visit(const details::body_node&) = 0;
+  virtual void visit(const details::class_node&) = 0;
+  virtual void visit(const details::program_node&) = 0;
+  virtual void visit(const details::expression_node&) = 0;
+  virtual void visit(const details::variable_node&) = 0;
+  virtual void visit(const details::method_node&) = 0;
+  virtual void visit(const details::constructor_node&) = 0;
+  virtual void visit(const details::assignment_node&) = 0;
+  virtual void visit(const details::while_loop_node&) = 0;
+  virtual void visit(const details::if_statement_node&) = 0;
+  virtual void visit(const details::return_statement_node&) = 0;
+  virtual void visit(const details::arguments_node&) = 0;
+  virtual void visit(const details::this_node&) = 0;
+  virtual void visit(const details::null_node&) = 0;
+  virtual void visit(const details::base_node&) = 0;
+  virtual void visit(const details::literal_node<int32_t>&) = 0;
+  virtual void visit(const details::literal_node<bool>&) = 0;
+  virtual void visit(const details::literal_node<double_t>&) = 0;
 
   virtual ~visitor() = default;
 };
@@ -73,17 +50,16 @@ class visitor {
 class scope_visitor : public visitor {
  private:
   std::shared_ptr<scope_checking::scope> scope_;
+  error_handling::error_handling error_;
 
  public:
-  void visit(const details::program_node& p,
-             error_handling::error_handling& e) override {
+  void visit(const details::program_node& p) override {
     for (const auto& cls : p.get_classes()) {
       cls->visit(this);
     }
   }
 
-  void visit(const details::class_node& cls,
-             error_handling::error_handling& e) override {
+  void visit(const details::class_node& cls) override {
     scope_ = scope_->push();
     for (const auto& m : cls.get_members()) {
       if (auto var = dynamic_cast<details::variable_node*>(m.get()); var) {
@@ -98,24 +74,22 @@ class scope_visitor : public visitor {
     scope_ = scope_->pop();
   }
 
-  void visit(const details::variable_node& v,
-             error_handling::error_handling& e) override {
+  void visit(const details::variable_node& v) override {
     const std::string& key = v.get_identifier()->get_name();
     auto found = scope_->find(key);
     if (found) {
-      e.register_error(make_error_t(v, "the class field is already declared"));
+      error_.register_error(make_error_t(v, "the class field is already declared"));
     } else {
       scope_->add(key, std::make_shared<details::variable_node>(v));
     }
   }
 
-  void visit(const details::method_node& m,
-             error_handling::error_handling& e) override {
+  void visit(const details::method_node& m) override {
     auto sym = std::make_shared<scope_checking::scope_symbol>();
     for (const auto& p : m.get_parameters()->get_parameters()) {
       std::string key = p->get_identifier()->get_name();
       if ((*sym)[key]) {
-        e.register_error(make_error_t(*p, VariableRedefinition));
+        error_.register_error(make_error_t(*p, VariableRedefinition));
         return;
       }
       (*sym)[key] = p;
@@ -125,7 +99,7 @@ class scope_visitor : public visitor {
       if (auto var = dynamic_cast<details::variable_node*>(s.get()); var) {
         std::string key = var->get_identifier()->get_name();
         if ((*sym)[key]) {
-          e.register_error(make_error_t(*s, VariableRedefinition));
+          error_.register_error(make_error_t(*s, VariableRedefinition));
           return;
         }
         (*sym)[key] = s;
@@ -136,8 +110,7 @@ class scope_visitor : public visitor {
     scope_ = scope_->pop();
   }
 
-  void visit(const details::expression_node& expr,
-             error_handling::error_handling& e) override {
+  void visit(const details::expression_node& expr) override {
     expr.get_primary()->visit(this);
     for (const auto& e : expr.get_expression_values()) {
       if (e.second) {
@@ -146,62 +119,51 @@ class scope_visitor : public visitor {
     }
   }
 
-  void visit(const details::class_name_node& c,
-             error_handling::error_handling& e) override {
+  void visit(const details::class_name_node& c) override {
     std::string key = c.get_identifier()->get_name();
     if (auto var = scope_->find(key); !var) {
-      e.register_error(make_error_t(c, "variable \"" + key + "\" undefined"));
+      error_.register_error(make_error_t(c, "variable \"" + key + "\" undefined"));
     }
   }
 
-  void visit(const details::primary_node& p,
-             error_handling::error_handling& e) override {
+  void visit(const details::primary_node& p) override {
     throw std::runtime_error("unsupported operation");
   }
 
-  void visit(const details::this_node& t,
-             error_handling::error_handling&) override {
+  void visit(const details::this_node& t) override {
     // this code block must be empty
   }
 
-  void visit(const details::null_node& n,
-             error_handling::error_handling&) override {
+  void visit(const details::null_node& n) override {
     // this code block must be empty
   }
 
-  void visit(const details::base_node& b,
-             error_handling::error_handling&) override {
+  void visit(const details::base_node& b) override {
     // this code block must be empty
   }
 
-  void visit(const details::literal_node<int32_t>& l,
-             error_handling::error_handling&) override {
+  void visit(const details::literal_node<int32_t>& l) override {
     // this code block must be empty
   }
 
-  void visit(const details::literal_node<bool>&,
-             error_handling::error_handling&) override {
+  void visit(const details::literal_node<bool>&) override {
     // this code block must be empty
   }
 
-  void visit(const details::literal_node<double_t>&,
-             error_handling::error_handling&) override {
+  void visit(const details::literal_node<double_t>&) override {
     // this code block must be empty
   }
 
-  void visit(const details::assignment_node& a,
-             error_handling::error_handling& e) override {
+  void visit(const details::assignment_node& a) override {
     // this code block must be empty
   }
 
-  void visit(const details::while_loop_node& w,
-             error_handling::error_handling& e) override {
+  void visit(const details::while_loop_node& w) override {
     w.get_expression()->visit(this);
     w.get_body_node()->visit(this);
   }
 
-  void visit(const details::if_statement_node& i,
-             error_handling::error_handling& e) override {
+  void visit(const details::if_statement_node& i) override {
     i.get_expression()->visit(this);
     i.get_true_body()->visit(this);
     if (auto false_body = i.get_false_body(); false_body) {
@@ -209,15 +171,14 @@ class scope_visitor : public visitor {
     }
   }
 
-  void visit(const details::body_node& b,
-             error_handling::error_handling& e) override {
+  void visit(const details::body_node& b) override {
     auto sym = std::make_shared<scope_checking::scope_symbol>();
     scope_ = scope_->push(sym);
     for (const auto& s : b.get_nodes()) {
       if (auto var = dynamic_cast<details::variable_node*>(s.get()); var) {
         std::string key = var->get_identifier()->get_name();
         if ((*sym)[key]) {
-          e.register_error(make_error_t(*s, VariableRedefinition));
+          error_.register_error(make_error_t(*s, VariableRedefinition));
           return;
         }
         (*sym)[key] = s;
