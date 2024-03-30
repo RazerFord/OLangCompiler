c9affce7098893f25dfc979ec8a595a821473e01
RazerFord <desk10567@gmail.com>
2024-03-30 18:33:42 +0300

    feat: added scope

--- src/semantic-analyzer/scope-checking.h
+++ src/semantic-analyzer/scope-checking.h
@@ -7,21 +7,41 @@
 
 namespace scope_checking {
 namespace {
-struct scope_symbol {
-  std::unordered_map<details::meta, std::shared_ptr<details::ast_node>> symbols;
+class scope_symbol {
+ private:
+  std::unordered_map<details::meta, std::shared_ptr<details::ast_node>>
+      symbols_;
+
+ public:
+  std::shared_ptr<details::ast_node> operator[](
+      const details::meta& key) const {
+    return symbols_.at(key);
+  }
 };
 }  // namespace
 
-class scope {
+class scope : public std::enable_shared_from_this<scope> {
  private:
-  std::shared_ptr<scope> parent;
-  std::shared_ptr<scope_symbol> symbols;
+  std::shared_ptr<scope> parent_;
+  std::shared_ptr<scope_symbol> symbol_;
 
  public:
+  scope() = default;
+
+  scope(std::shared_ptr<scope> parent) : parent_{std::move(parent)} {}
+
+  scope(std::shared_ptr<scope> parent, std::shared_ptr<scope_symbol> symbol)
+      : parent_{std::move(parent)}, symbol_{std::move(symbol)} {}
+
+  scope(std::shared_ptr<scope_symbol> symbol)
+      : parent_{}, symbol_{std::move(symbol)} {}
+
   std::shared_ptr<scope> push();
+
   std::shared_ptr<scope> push(std::shared_ptr<scope_symbol> symbols);
+
   std::shared_ptr<scope> pop();
+
   std::shared_ptr<details::ast_node> find(details::meta);
 };
-
 }  // namespace scope_checking
