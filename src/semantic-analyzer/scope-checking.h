724e1b3e655b4c2ae2f259af3342c5a03cc12f76
RazerFord <desk10567@gmail.com>
2024-03-30 20:08:13 +0300

    feat: added operator[]

--- src/semantic-analyzer/scope-checking.h
+++ src/semantic-analyzer/scope-checking.h
@@ -9,20 +9,47 @@ namespace scope_checking {
 namespace {
 class scope_symbol {
  private:
-  std::unordered_map<details::meta, std::shared_ptr<details::ast_node>>
-      symbols_;
+  struct proxy {
+   private:
+    std::string key_;
+    scope_symbol* scope_symbol_;
 
- public:
-  std::shared_ptr<details::ast_node> operator[](
-      const details::meta& key) const {
+   public:
+    proxy(std::string key, scope_symbol* scope_symbol_)
+        : key_{key}, scope_symbol_{scope_symbol_} {}
+
+    std::shared_ptr<details::ast_node> operator=(
+        std::shared_ptr<details::ast_node> value) {
+      return scope_symbol_->symbols_[key_] = value;
+    }
+
+    operator bool() {
+      const auto& syms = scope_symbol_->symbols_;
+      return (syms.find(key_) != syms.end());
+    }
+
+    operator std::shared_ptr<details::ast_node>() const {
+      return scope_symbol_->tryFind(key_);
+    }
+  };
+
+  std::shared_ptr<details::ast_node> tryFind(const std::string& key) const {
     auto it = symbols_.find(key);
-
     if (it == symbols_.end()) {
       return {};
     }
-
     return it->second;
   }
+
+ private:
+  std::unordered_map<std::string, std::shared_ptr<details::ast_node>> symbols_;
+
+ public:
+  std::shared_ptr<details::ast_node> operator[](const std::string& key) const {
+    return tryFind(key);
+  }
+
+  proxy operator[](const std::string& key) { return proxy(key, this); }
 };
 }  // namespace
 
@@ -48,21 +75,24 @@ class scope : public std::enable_shared_from_this<scope> {
 
   std::shared_ptr<scope> pop();
 
-  std::shared_ptr<details::ast_node> find(const details::meta&);
+  std::shared_ptr<details::ast_node> find(const std::string&);
+
+  std::shared_ptr<details::ast_node> add(const std::string&,
+                                         std::shared_ptr<details::ast_node>);
 };
 
 inline std::shared_ptr<scope> scope::push() {
   return std::make_shared<scope>(shared_from_this());
 }
 
-inline std::shared_ptr<scope> scope::push(std::shared_ptr<scope_symbol> symbols) {
+inline std::shared_ptr<scope> scope::push(
+    std::shared_ptr<scope_symbol> symbols) {
   return std::make_shared<scope>(shared_from_this(), symbols);
 }
 
 inline std::shared_ptr<scope> scope::pop() { return parent_; }
 
-inline std::shared_ptr<details::ast_node> scope::find(
-    const details::meta& key) {
+inline std::shared_ptr<details::ast_node> scope::find(const std::string& key) {
   for (std::shared_ptr<scope> scope = this->shared_from_this(); scope;
        scope = scope->parent_) {
     if (auto value = (*scope->symbol_)[key]; value) {
@@ -70,5 +100,10 @@ inline std::shared_ptr<details::ast_node> scope::find(
     }
   }
   return nullptr;
+}
+
+inline std::shared_ptr<details::ast_node> scope::add(
+    const std::string& key, std::shared_ptr<details::ast_node> value) {
+  return (*symbol_)[key] = value;
 }
 }  // namespace scope_checking
--- src/semantic-analyzer/visitor.h
+++ src/semantic-analyzer/visitor.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 
+#include "./../logging/error_handler.h"
 #include "scope-checking.h"
 #include "syntactical-analyzer/details.h"
 
@@ -9,45 +10,45 @@ namespace visitor {
 class visitor {
  public:
   virtual void visit(const details::identifier_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::primary_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::class_name_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::parameter_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::parameters_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::body_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::class_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::program_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::expression_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::variable_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::method_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::constructor_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::assignment_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::while_loop_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::if_statement_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::return_statement_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::arguments_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::this_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::null_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
   virtual void visit(const details::base_node&,
-                     const error_handling::error_handling&) = 0;
+                     error_handling::error_handling&) = 0;
 
   virtual ~visitor() = default;
 };
@@ -58,9 +59,38 @@ class scope_visitor : public visitor {
 
  public:
   void visit(const details::program_node& p,
-             const error_handling::error_handling& e) override {
+             error_handling::error_handling& e) override {
     for (const auto& cls : p.get_classes()) {
       cls->visit(this);
+    }
+  }
+
+  void visit(const details::class_node& cls,
+             error_handling::error_handling& e) override {
+    scope_ = scope_->push();
+    for (const auto& m : cls.get_members()) {
+      if (auto var = dynamic_cast<details::variable_node*>(m.get()); var) {
+        var->visit(this);
+      }
+    }
+    for (const auto& m : cls.get_members()) {
+      if (auto var = dynamic_cast<details::method_node*>(m.get()); var) {
+        var->visit(this);
+      }
+    }
+    scope_ = scope_->pop();
+  }
+
+  void visit(const details::variable_node& v,
+             error_handling::error_handling& e) override {
+    const std::string& key = v.get_identifier()->get_name();
+    auto found = scope_->find(key);
+    if (found) {
+      error_handling::error_t et(v.get_meta_info(),
+                                 "the class field is already declared");
+      e.register_error(et);
+    } else {
+      scope_->add(key, std::make_shared<details::variable_node>(v));
     }
   }
 };
