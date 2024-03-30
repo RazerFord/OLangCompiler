d8b02b315f9b019047a2f557ea5928b9702bf6b3
AvvALlV <cghjhfdshj@gmail.com>
2024-03-31 01:12:43 +0300

    feat: added mangle in ctr

--- src/syntactical-analyzer/details.h
+++ src/syntactical-analyzer/details.h
@@ -206,7 +206,7 @@ class class_name_node : public primary_node {
     }
   }
 
-  std::string mangle_class_name() {
+  std::string mangle_class_name() const {
     auto name = identifier_->get_name();
     std::string generic_mangle;
     if (generic_) {
@@ -264,7 +264,7 @@ class parameter_node : public ast_node {
     }
   }
 
-  std::string mangle_param() { return class_name_->mangle_class_name(); }
+  std::string mangle_param() const { return class_name_->mangle_class_name(); }
 };
 
 class parameters_node : public ast_node {
@@ -311,7 +311,7 @@ class parameters_node : public ast_node {
     }
   }
 
-  std::string mangle_parameters() {
+  std::string mangle_parameters() const {
     if (parameters_.empty()) {
       return "v";
     }
@@ -685,7 +685,7 @@ class method_node : public member_node {
     std::cout << "end";
   }
 
-  std::string mangle_method() {
+  std::string mangle_method() const {
     return std::to_string(identifier_->get_name().size()) +
            identifier_->get_name() + "E" + parameters_->mangle_parameters();
   }
@@ -738,6 +738,10 @@ class constructor_node : public member_node {
     std::cout << ") is\n";
     body_->print();
     std::cout << "end";
+  }
+
+  std::string mangle_ctr() const {
+    return "C" + parameters_->mangle_parameters();
   }
 };
 
