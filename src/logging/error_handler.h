2ca361f319c8e62ac799127e508a4ac7ddac55fe
AvvALlV <cghjhfdshj@gmail.com>
2024-03-30 17:43:07 +0300

    feat: added error_handler

---
+++ src/logging/error_handler.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "syntactical-analyzer/details.h"
+
+namespace error_handling {
+
+struct error_t {
+  details::meta metadata;
+  std::string error_message;
+};
+
+class error_handling {
+  std::vector<error_t> error_metadata_;
+
+ public:
+  void register_error(const error_t& err) { error_metadata_.push_back(err); }
+  void print_errors() {
+    for (auto& err: error_metadata_) {
+        auto& span =  err.metadata.span_;
+        std::cout << span.line_ << ":" << span.offset_ << " " << err.error_message;
+    }
+  }
+};
+}  // namespace error_handling
