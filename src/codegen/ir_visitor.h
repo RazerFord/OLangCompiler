f42181c6598ce7471b59672dbb5128adc01cce69
RazerFord <desk10567@gmail.com>
2024-04-20 18:54:45 +0300

    feat: start todo codegen

--- src/codegen/ir_visitor.h
+++ src/codegen/ir_visitor.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <sstream>
+
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
@@ -7,7 +9,8 @@
 #include "visitor/visitor.h"
 
 inline constexpr std::string ModuleName = "Main";
-inline constexpr std::string PrefixVTable = "__VT";
+inline constexpr std::string PrefixNameMemberVar = "_var";
+inline constexpr std::string PrefixVTable = "__VTable";
 
 namespace ir_visitor {
 class ir_visitor : public visitor::visitor {
@@ -27,7 +30,68 @@ class ir_visitor : public visitor::visitor {
       name = PrefixVTable + c->get_class_name()->get_identifier()->get_name();
       llvm::StructType::create(*ctx_, llvm::StringRef(name));
     }
+    for (const auto& c : p.get_classes()) {
+      std::string name = c->get_class_name()->get_identifier()->get_name();
+      llvm::StructType* ptrCls = module_->getTypeByName(llvm::StringRef(name));
+      name = PrefixVTable + c->get_class_name()->get_identifier()->get_name();
+      llvm::PointerType* ptrVTable =
+          module_->getTypeByName(llvm::StringRef(name))->getPointerTo();
 
+      std::vector<llvm::Type*> body{ptrVTable};
+      member_variable_visitor mvv(&body, this);
+      for (const auto& m : c->get_members()) {
+        m->visit(&mvv);
+      }
+      ptrCls->setBody(llvm::ArrayRef(body));
+    }
+    std::string outstr;
+    llvm::raw_string_ostream oss(outstr);
+
+    module_->print(oss, nullptr, true, true);
+    std::cout << outstr << std::endl;
   }
+
+ private:
+  class member_variable_visitor : public visitor::visitor {
+   private:
+    std::vector<llvm::Type*>* const body_ = nullptr;
+    const ir_visitor* ir_visitor_ = nullptr;
+    int count = 0;
+
+   public:
+    explicit member_variable_visitor(std::vector<llvm::Type*>* const body,
+                                     const ir_visitor* ir_visitor)
+        : body_{body}, ir_visitor_{ir_visitor} {}
+
+    void visit(details::variable_node& v) override {
+      std::string cls_name{
+          v.get_type()->get_class_name()->get_identifier()->get_name()};
+      llvm::Type* ptrCls =
+          ir_visitor_->module_->getTypeByName(llvm::StringRef(cls_name))
+              ->getPointerTo();
+      body_->push_back(ptrCls);
+    }
+  };
+
+  class member_method_visitor : public visitor::visitor {
+   private:
+    std::vector<llvm::Type*>* const body_ = nullptr;
+    const ir_visitor* ir_visitor_ = nullptr;
+    int count = 0;
+
+   public:
+    explicit member_method_visitor(std::vector<llvm::Type*>* const body,
+                                     const ir_visitor* ir_visitor)
+        : body_{body}, ir_visitor_{ir_visitor} {}
+
+    void visit(details::variable_node& v) override {
+      std::string cls_name{
+          v.get_type()->get_class_name()->get_identifier()->get_name()};
+      llvm::Type* ptrCls =
+          ir_visitor_->module_->getTypeByName(llvm::StringRef(cls_name))
+              ->getPointerTo();
+      body_->push_back(ptrCls);
+    }
+  };
 };
 }  // namespace ir_visitor
